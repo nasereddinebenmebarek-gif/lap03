d7a1fde68b8728fcb6d3957c60d960cd361af6d2
nasereddinebenmebarek-gif <nasereddinebenmebarek@gmail.com>
2026-04-19 20:28:43 +0100

    Update user.php

--- step01/user.php
+++ step01/user.php
@@ -1,11 +1,2 @@
 <?php
-require_once "config.php";
-
-class User {
-    public static function findByEmail($email) {
-        global $pdo;
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
-        $stmt->execute([$email]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-}
+echo password_hash("1234", PASSWORD_BCRYPT);
