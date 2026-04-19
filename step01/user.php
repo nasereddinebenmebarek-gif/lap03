ddea9333b190d5dd7877c08cee56b5d2780b795a
nasereddinebenmebarek-gif <nasereddinebenmebarek@gmail.com>
2026-04-19 19:16:24 +0100

    Add User class with findByEmail method

---
+++ step01/user.php
@@ -0,0 +1,11 @@
+<?php
+require_once "config.php";
+
+class User {
+    public static function findByEmail($email) {
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
