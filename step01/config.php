0891040c29b7c85ccc25e82b7f5e8cf929478df3
nasereddinebenmebarek-gif <nasereddinebenmebarek@gmail.com>
2026-04-19 20:25:48 +0100

    Update config.php

--- step01/config.php
+++ step01/config.php
@@ -1,21 +1,17 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=gpa_system", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo = new PDO("mysql:host=localhost;dbname=gpa_db", "root", "");
 
 function requireRole($role) {
     session_start();
 
-    if (!isset($_SESSION['role']) || time() - $_SESSION['last_activity'] > 1800) {
-        session_destroy();
-        header("Location: ?page=login");
+    if (!isset($_SESSION['role'])) {
+        header("Location: index.php?page=login");
         exit;
     }
 
     if ($_SESSION['role'] != $role) {
-        http_response_code(403);
         echo "Access Denied";
         exit;
     }
-
-    $_SESSION['last_activity'] = time();
 }
+?>
