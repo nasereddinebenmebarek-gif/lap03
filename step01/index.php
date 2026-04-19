0f545cb4401569336ed940f10f447d499a3500a1
nasereddinebenmebarek-gif <nasereddinebenmebarek@gmail.com>
2026-04-19 20:27:21 +0100

    Update index.php

--- step01/index.php
+++ step01/index.php
@@ -1,17 +1,34 @@
 <?php
-session_start();
+require_once "controllers/AuthController.php";
+require_once "controllers/AdminController.php";
 
 $page = $_GET['page'] ?? 'login';
 
-require_once "controllers/AuthController.php";
+switch($page) {
 
-switch ($page) {
     case 'login':
         (new AuthController())->login();
         break;
 
-    case 'logout':
-        (new AuthController())->logout();
+    case 'admin':
+    case 'admin.dashboard':
+        (new AdminController())->dashboard();
+        break;
+
+    case 'admin.semesters':
+        (new AdminController())->semesters();
+        break;
+
+    case 'admin.saveSemester':
+        (new AdminController())->saveSemester();
+        break;
+
+    case 'admin.courses':
+        (new AdminController())->courses();
+        break;
+
+    case 'admin.saveCourse':
+        (new AdminController())->saveCourse();
         break;
 
     default:
